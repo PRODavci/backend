45ea8a8667315c89edc4226e2280d2ddb7b31754
Andrey Bakov <andrey.bakov.2006@gmail.com>
2024-11-17 17:10:51 +0300

    add change password test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,13 +7,10 @@ async def test_registration(async_client):
         "email": "email.test@mail.ru",
         "password": "superstrongpassword",
     }
-    # Отправка POST-запроса
     response = await async_client.post("/api/v1/users", json=data)
 
-    # Проверка статуса ответа
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}, body: {response.text}"
 
-    # Проверка структуры ответа
     response_data = response.json()
     assert "user" in response_data, f"Response body: {response_data}"
     assert "tokens" in response_data, f"Response body: {response_data}"
@@ -26,11 +23,9 @@ async def test_login(async_client):
         "password": "superstrongpassword",
     }
 
-    # Отправка запроса на создание пользователя
     response = await async_client.post("/api/v1/users", json=data)
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}, body: {response.text}"
 
-    # Отправка запроса на логин
     response = await async_client.post("/api/v1/users/login", json=data)
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}, body: {response.text}"
 
@@ -38,8 +33,48 @@ async def test_login(async_client):
     assert "user" in response_data, f"Response body: {response_data}"
     assert "tokens" in response_data, f"Response body: {response_data}"
 
-    # Проверка маршрута /me
     token = response_data["tokens"]["access_token"]
-    print(token)
+
+    response = await async_client.get("/api/v1/users/me", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}, body: {response.text}"
+
+
+async def test_change_password(async_client):
+    data = {
+        "email": "email.test@mail.ru",
+        "password": "superstrongpassword",
+    }
+
+    response = await async_client.post("/api/v1/users", json=data)
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}, body: {response.text}"
+
+    token = response.json()["tokens"]["access_token"]
+
+    response = await async_client.get("/api/v1/users/me", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}, body: {response.text}"
+
+    data = {
+        "old_password": "superstrongpassword",
+        "new_password": "notsuperstrongpassword",
+    }
+
+    response = await async_client.post(
+        "/api/v1/users/change_password", json=data, headers={"Authorization": f"Bearer {token}"}
+    )
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}, body: {response.text}"
+
+    response = await async_client.get("/api/v1/users/me", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 401, f"Unexpected status code: {response.status_code}, body: {response.text}"
+
+    data = {
+        "email": "email.test@mail.ru",
+        "password": "notsuperstrongpassword",
+    }
+
+    response = await async_client.post("/api/v1/users/login", json=data)
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}, body: {response.text}"
+
+    token = response.json()["tokens"]["access_token"]
+
     response = await async_client.get("/api/v1/users/me", headers={"Authorization": f"Bearer {token}"})
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}, body: {response.text}"
